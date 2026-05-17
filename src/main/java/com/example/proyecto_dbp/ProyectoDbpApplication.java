44c0c4eb598bf64c62921a88aa674e286b350778
Jean <jean.arana@utec.edu.pe>
2026-05-17 03:45:49 -0500

    Primer commit

---
+++ src/main/java/com/example/proyecto_dbp/ProyectoDbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_dbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoDbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyecto_dbp/ProyectoDbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_dbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
